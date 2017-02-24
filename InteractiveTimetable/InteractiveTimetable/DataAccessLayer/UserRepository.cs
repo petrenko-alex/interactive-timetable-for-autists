75caea25fe4ff85f1f10dc2b0717846abf3d8c9c
Александр Петренко <gafk555@gmail.com>
2017-02-24 15:49:13 +0300

    Change to cascade operations in UserRepository

--- InteractiveTimetable/InteractiveTimetable/DataAccessLayer/UserRepository.cs
+++ InteractiveTimetable/InteractiveTimetable/DataAccessLayer/UserRepository.cs
@@ -18,22 +18,27 @@ namespace InteractiveTimetable.DataAccessLayer
 
         public User GetUser(int id)
         {
-            return _database.GetItem<User>(id);
+            return _database.GetItemCascade<User>(id);
         }
 
         public IEnumerable<User> GetUsers()
         {
-            return _database.GetItems<User>();
+            return _database.GetItemsCascade<User>();
         }
 
         public int SaveUser(User user)
         {
-            return _database.SaveItem(user);
+            return _database.SaveItemCascade(user);
         }
 
         public int DeleteUser(int id)
         {
             return _database.DeleteItem<User>(id);
+        }
+
+        public void DeleteUserCascade(User user)
+        {
+            _database.DeleteItemCascade(user);
         }
     }
 }
