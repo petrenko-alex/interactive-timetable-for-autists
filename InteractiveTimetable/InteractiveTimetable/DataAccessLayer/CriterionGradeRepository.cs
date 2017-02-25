afeba61a93769ad4c6c3a1d20a6466eed2f542da
Александр Петренко <gafk555@gmail.com>
2017-02-25 22:23:20 +0300

    Start implementing CriterionGradeRepository

--- InteractiveTimetable/InteractiveTimetable/DataAccessLayer/CriterionGradeRepository.cs
+++ InteractiveTimetable/InteractiveTimetable/DataAccessLayer/CriterionGradeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using InteractiveTimetable.BusinessLayer.Models;
 using SQLite;
 
@@ -6,9 +7,14 @@ namespace InteractiveTimetable.DataAccessLayer
 {
     internal class CriterionGradeRepository : BaseRepository
     {
-        public CriterionGradeRepository(SQLiteConnection connection) : base(connection)
-        {
-        }
+        private CriterionDefinitionRepository _criterionDefinitionRepository;
+
+        private const int MinimumPointGrade = 1;
+        private const int MaximumPointGrade = 4;
+        private const int MaximumLengthOfTickGradeString = 4;
+
+        public CriterionGradeRepository(SQLiteConnection connection)
+            : base(connection) {}
 
         public CriterionGrade GetCriterionGrade(int id)
         {
@@ -20,14 +26,70 @@ namespace InteractiveTimetable.DataAccessLayer
             return _database.GetItems<CriterionGrade>();
         }
 
-        public int SaveCriterionGrade(CriterionGrade criterionGrade)
+        public int SaveCriterionGrade(CriterionGrade grade)
         {
-            return _database.SaveItem(criterionGrade);
+            /* Data validation */
+            Validate(grade);
+
+            return _database.SaveItem(grade);
         }
 
         public int DeleteCriterion(int id)
         {
             return _database.DeleteItem<CriterionGrade>(id);
+        }
+
+        public CriterionGradeType GetCriterionGradeType(CriterionGrade grade)
+        {
+            var criterionId = grade.CriterionDefinitionId;
+            var criterion = _criterionDefinitionRepository.
+                    GetCriterionDefinition(criterionId);
+
+            return _criterionDefinitionRepository.
+                    GetCriterionGradeType(criterion);
+        }
+
+        private void Validate(CriterionGrade grade)
+        {
+            var criterionId = grade.CriterionDefinitionId;
+            var criterion = _criterionDefinitionRepository.
+                    GetCriterionDefinition(criterionId);
+
+            bool isPointGradeType = _criterionDefinitionRepository.
+                    IsPointGradeTypeCriterion(criterion);
+
+            bool isTickGradeType = _criterionDefinitionRepository.
+                    IsTickGradeTypeCriterion(criterion);
+
+            if (isPointGradeType)
+            {
+                int numberGrade;
+
+                if (int.TryParse(grade.Grade, out numberGrade))
+                {
+                    if (numberGrade < MinimumPointGrade ||
+                        numberGrade > MaximumPointGrade)
+                    {
+                        throw new ArgumentException("Not valid grade.");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Not valid grade.");
+                }
+            }
+            else if (isTickGradeType)
+            {
+                if (grade.Grade.Length < 1 ||
+                    grade.Grade.Length > MaximumLengthOfTickGradeString)
+                {
+                    throw new ArgumentException("Not valid grade.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Not valid grade type.");
+            }
         }
     }
 }
