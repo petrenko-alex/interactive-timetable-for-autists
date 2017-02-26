b9a996e5d4ebd8a2593c4a083ab3529b6bb164eb
Александр Петренко <gafk555@gmail.com>
2017-02-26 18:14:34 +0300

    Start implementing DiagnosticManager

---
+++ InteractiveTimetable/InteractiveTimetable/BusinessLayer/Managers/DiagnosticManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InteractiveTimetable.BusinessLayer.Models;
+using InteractiveTimetable.DataAccessLayer;
+using SQLite;
+
+namespace InteractiveTimetable.BusinessLayer.Managers
+{
+    public class DiagnosticManager
+    {
+        private readonly DiagnosticRepository _repository;
+
+        public DiagnosticManager(SQLiteConnection connection)
+        {
+            _repository = new DiagnosticRepository(connection);
+        }
+
+        public Diagnostic GetDiagnostic(int diagnosticId)
+        {
+            return _repository.GetDiagnostic(diagnosticId);
+        }
+
+        public IEnumerable<Diagnostic> GetDiagnostics(int hospitalTripId)
+        {
+            return _repository.GetTripDiagnostics(hospitalTripId);
+        }
+
+        //public int SaveDiagnostic() {}
+
+        public void DeleteDiagnostic(int diagnosticId)
+        {
+            var diagnostic = GetDiagnostic(diagnosticId);
+            _repository.DeleteDiagnosticCascade(diagnostic);
+        }
+
+//        public int GetTotalSum(int diagnosticId)
+//        {
+//            var grades = GetDiagnostic(diagnosticId).CriterionGrades;
+//
+//            grades.Where(x => _repository.Grades.CriterionDefinitions.
+//                                          IsPointGradeTypeCriterion(
+//                                              x.CriterionDefinitionId));
+//        }
+
+        //public int GetPartialSum(int grade) {}
+
+        public IEnumerable<string> GetCriterions()
+        {
+            var criterionDefinitions = _repository.Grades.CriterionDefinitions.
+                                                   GerCriterionDefinitions();
+
+            return criterionDefinitions.Select(x => x.Definition);
+        }
+    }
+}
