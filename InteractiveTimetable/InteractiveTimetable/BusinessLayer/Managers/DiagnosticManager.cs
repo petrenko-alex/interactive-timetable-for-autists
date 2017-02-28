48258383128cef0597298f065e4358ae2c61c56c
Александр Петренко <gafk555@gmail.com>
2017-02-28 21:48:54 +0300

    Implement methods for getting sum of diagnostic grades

--- InteractiveTimetable/InteractiveTimetable/BusinessLayer/Managers/DiagnosticManager.cs
+++ InteractiveTimetable/InteractiveTimetable/BusinessLayer/Managers/DiagnosticManager.cs
@@ -36,16 +36,15 @@ namespace InteractiveTimetable.BusinessLayer.Managers
             _repository.DeleteDiagnosticCascade(diagnostic);
         }
 
-//        public int GetTotalSum(int diagnosticId)
-//        {
-//            var grades = GetDiagnostic(diagnosticId).CriterionGrades;
-//
-//            grades.Where(x => _repository.Grades.CriterionDefinitions.
-//                                          IsPointGradeTypeCriterion(
-//                                              x.CriterionDefinitionId));
-//        }
+        public int GetTotalSum(int diagnosticId)
+        {
+            return GetNumberGrades(diagnosticId).Sum();
+        }
 
-        //public int GetPartialSum(int grade) {}
+        public int GetPartialSum(int diagnosticId, int grade)
+        {
+            return GetNumberGrades(diagnosticId).Where(x => x == grade).Sum();
+        }
 
         public IEnumerable<string> GetCriterions()
         {
@@ -53,6 +52,21 @@ namespace InteractiveTimetable.BusinessLayer.Managers
                                                    GerCriterionDefinitions();
 
             return criterionDefinitions.Select(x => x.Definition);
+        }
+
+        private IEnumerable<int> GetNumberGrades(int diagnosticId)
+        {
+            /* Getting all diagnostic grades as objects */
+            var grades = GetDiagnostic(diagnosticId).CriterionGrades;
+
+            /* Getting grades that have point grade type as objects */
+            var pointGrades = grades.Where(
+                x => _repository.Grades.CriterionDefinitions.
+                                          IsPointGradeTypeCriterion(
+                                              x.CriterionDefinitionId));
+
+            /* Getting grades as numbers */
+            return pointGrades.Select(x => Convert.ToInt32(x.Grade));
         }
     }
 }
