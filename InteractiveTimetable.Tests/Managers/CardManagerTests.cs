26b7c063d88b9f0d5d971680f48da4ce9096ece6
Александр Петренко <gafk555@gmail.com>
2017-03-05 23:58:56 +0300

    Add first tests to CardManagerTests

--- InteractiveTimetable.Tests/Managers/CardManagerTests.cs
+++ InteractiveTimetable.Tests/Managers/CardManagerTests.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using InteractiveTimetable.BusinessLayer.Models;
 using InteractiveTimetable.DataAccessLayer;
 using NUnit.Framework;
@@ -11,6 +13,16 @@ namespace InteractiveTimetable.Tests.Managers
     {
         private SQLiteConnection _connection;
         private CardRepository _repository;
+
+        public string GenerateRandomString(int stringLength)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            return new string(Enumerable.Repeat(chars, stringLength).
+                                         Select(s => s[random.Next(s.Length)]).
+                                         ToArray());
+        }
 
         [SetUp]
         public void InitializationBeforeTests()
@@ -34,15 +46,47 @@ namespace InteractiveTimetable.Tests.Managers
         public void CreateSimpleCard()
         {
             // arrange
+            CardType activityType = _repository.CardTypes.GetActivityCardType();
             Card card = new Card()
             {
                 PhotoPath = "path/to/photo.jpg",
-                CardTypeId = 1
+                CardTypeId = activityType.Id
             };
 
             // act
             var cardId = _repository.SaveCard(card);
+            var addedCard = _repository.GetCard(cardId);
 
+            // assert
+            Assert.AreEqual(card, addedCard);
+        }
+
+        [Test]
+        public void CreateCardWithIncorrectPath()
+        {
+            // arrange
+            CardType activityType = _repository.CardTypes.GetActivityCardType();
+            Card card = new Card()
+            {
+                PhotoPath = GenerateRandomString(1025),
+                CardTypeId = activityType.Id
+            };
+            string exceptionString = "The length of the path " +
+                                     "to the card's photo must be less " +
+                                     "than 1024 symbols";
+
+            // act/assert
+            var exception = Assert.Throws<ArgumentException>(delegate
+            {
+                _repository.SaveCard(card);
+            });
+            Assert.AreEqual(exceptionString, exception.Message);
+        }
+
+        [Test]
+        public void CreateCardWithScheduleItems()
+        {
+            // TODO: Implement when ScheduleItemRepository is done
         }
     }
 }
