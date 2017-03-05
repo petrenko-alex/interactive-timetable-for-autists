d3fd67e1e3b52c7541f59eda7fb80c62d4494305
Александр Петренко <gafk555@gmail.com>
2017-03-05 15:28:57 +0300

    Start implementing CardRepository

--- InteractiveTimetable/InteractiveTimetable/DataAccessLayer/CardRepository.cs
+++ InteractiveTimetable/InteractiveTimetable/DataAccessLayer/CardRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 using InteractiveTimetable.BusinessLayer.Models;
 using SQLite;
 
@@ -6,28 +8,48 @@ namespace InteractiveTimetable.DataAccessLayer
 {
     internal class CardRepository : BaseRepository
     {
+        private readonly CardTypeRepository CardTypes;
+
         public CardRepository(SQLiteConnection connection) : base(connection)
         {
+            CardTypes = new CardTypeRepository(connection);
         }
 
-        public Card GetCard(int id)
+        public Card GetCard(int cardId)
         {
-            return _database.GetItem<Card>(id);
+            return _database.GetItemCascade<Card>(cardId);
         }
 
         public IEnumerable<Card> GetCards()
         {
-            return _database.GetItems<Card>();
+            return _database.GetItemsCascade<Card>();
         }
 
         public int SaveCard(Card card)
         {
-            return _database.SaveItem(card);
+            return _database.SaveItemCascade(card);
         }
 
-        public int DeleteCard(int id)
+        public int DeleteCard(int cardId)
         {
-            return _database.DeleteItem<Card>(id);
+            return _database.DeleteItem<Card>(cardId);
+        }
+
+        public void DeleteCardCascade(Card card)
+        {
+            _database.DeleteItemCascade(card);
+        }
+
+        public IEnumerable<Card> GetActivityCards()
+        {
+            return GetCards().
+                    Where(x => CardTypes.IsActivityCardType(x.CardTypeId));
+        }
+
+        public IEnumerable<Card> GetMotivationGoalCards()
+        {
+            return GetCards().
+                    Where(x => CardTypes.IsMotivationGoalCardType(x.CardTypeId));
         }
     }
 }
