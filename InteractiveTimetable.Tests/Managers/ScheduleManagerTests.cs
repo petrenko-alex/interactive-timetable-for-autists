ff28f97853f1fe48d6140eae99dea960cd5a08b1
Александр Петренко <gafk555@gmail.com>
2017-03-09 17:43:13 +0300

    Add tests for ScheduleManager

--- InteractiveTimetable.Tests/Managers/ScheduleManagerTests.cs
+++ InteractiveTimetable.Tests/Managers/ScheduleManagerTests.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using InteractiveTimetable.BusinessLayer.Managers;
 using InteractiveTimetable.BusinessLayer.Models;
@@ -78,13 +80,306 @@ namespace InteractiveTimetable.Tests.Managers
         public void CreateSimpleSchedule()
         {
             // arrange
-            var activityCards = _manager.Cards.GetActivityCards().ToList();
-            var goalCards = _manager.Cards.GetMotivationGoalCards().ToList();
+            var activityCards = _manager.Cards.GetActivityCards().
+                                         Select(x => x.Id).
+                                         ToList();
 
+            var goalCards = _manager.Cards.GetMotivationGoalCards().
+                                     Select(x => x.Id).
+                                     ToList();
+
+            var ids = activityCards.Concat(goalCards).ToList();
+
+            // act
+            var scheduleId = _manager.SaveSchedule(_user.Id, ids);
+            var addedSchedule = _manager.GetSchedule(scheduleId);
+
+            // assert
+            Assert.AreEqual(ids.Count, addedSchedule.ScheduleItems.Count);
+            Assert.AreEqual(1, addedSchedule.ScheduleItems[0].CardId);
+            Assert.AreEqual(2, addedSchedule.ScheduleItems[1].CardId);
+            Assert.AreEqual(3, addedSchedule.ScheduleItems[2].CardId);
+            Assert.AreEqual(4, addedSchedule.ScheduleItems[3].CardId);
+            Assert.AreEqual(_user.Id, addedSchedule.UserId);
+        }
+
+        [Test]
+        public void CreateScheduleWithNullListOfIds()
+        {
+            // arrange
+            var exceptionString = "Card ids are not set.";
+
+            // act/assert
+            var exception = Assert.Throws<ArgumentException>(delegate
+            {
+                _manager.SaveSchedule(_user.Id, null);
+            });
+            Assert.AreEqual(exceptionString, exception.Message);
+        }
+
+        [Test]
+        public void CreateScheduleWithNotExistingCards()
+        {
+            // arrange
+            var activityCards = _manager.Cards.GetActivityCards().
+                                         Select(x => x.Id).
+                                         ToList();
+
+            var goalCards = _manager.Cards.GetMotivationGoalCards().
+                                     Select(x => x.Id).
+                                     ToList();
+
+            var ids = activityCards.Concat(goalCards).ToList();
+            ids.Add(33);
+            var exceptionString = "Card with id: \"33\" doesn't exist.";
+
+            // act/assert
+            var exception = Assert.Throws<ArgumentException>(delegate
+            {
+                _manager.SaveSchedule(_user.Id, ids);
+            });
+            Assert.AreEqual(exceptionString, exception.Message);
+        }
+
+        [Test]
+        public void CreateScheduleWithoutActivityCards()
+        {
+            // arrange
+            var goalCards = _manager.Cards.GetMotivationGoalCards().
+                                     Select(x => x.Id).
+                                     ToList();
+            var exceptionString = "Activity card is not set " +
+                                  "in timetable. At least one " +
+                                  "activity card must be set.";
+            // act/assert
+            var exception = Assert.Throws<ArgumentException>(delegate
+            {
+                _manager.SaveSchedule(_user.Id, goalCards);
+            });
+            Assert.AreEqual(exceptionString, exception.Message);
+        }
+
+        [Test]
+        public void CreateScheduleWithoutMotivationGoalCards()
+        {
+            // arrange
+            var activityCards = _manager.Cards.GetActivityCards().
+                                         Select(x => x.Id).
+                                         ToList();
+            var exceptionString = "Motivation goal card is not set" +
+                                  "in timetable. You need to set " +
+                                  "one motivation goal card.";
+
+            // act/assert
+            var exception = Assert.Throws<ArgumentException>(delegate
+            {
+                _manager.SaveSchedule(_user.Id, activityCards);
+            });
+            Assert.AreEqual(exceptionString, exception.Message);
+        }
+
+        [Test]
+        public void CreateScheduleWithEmptyListOfIds()
+        {
+            // arrange
+            var exceptionString = "Card ids are not set.";
+
+            // act/assert
+            var exception = Assert.Throws<ArgumentException>(delegate
+            {
+                _manager.SaveSchedule(_user.Id, new List<int>());
+            });
+            Assert.AreEqual(exceptionString, exception.Message);
+        }
+
+        [Test]
+        public void CreateScheduleWithMultipleMotivationGoalCards()
+        {
+            // arrange
+            var goalType = _manager.Cards.CardTypes.GetMotivationGoalCardType();
+            Card card = new Card()
+            {
+                PhotoPath = "path/to/photo5.jpg",
+                CardTypeId = goalType.Id
+            };
+            _manager.Cards.SaveCard(card);
+
+            var activityCards = _manager.Cards.GetActivityCards().
+                                         Select(x => x.Id).
+                                         ToList();
+
+            var goalCards = _manager.Cards.GetMotivationGoalCards().
+                                     Select(x => x.Id).
+                                     ToList();
+
+            var ids = activityCards.Concat(goalCards).ToList();
+
+            var exceptionString = "Multiple motivation goal " +
+                                  "cards are not allowed.";
+
+            // act/assert
+            var exception = Assert.Throws<ArgumentException>(delegate
+            {
+                _manager.SaveSchedule(_user.Id, ids);
+            });
+            Assert.AreEqual(exceptionString, exception.Message);
+        }
+
+        [Test]
+        public void GetAllSchedules()
+        {
+            // arrange
+            var activityCards = _manager.Cards.GetActivityCards().
+                                         Select(x => x.Id).
+                                         ToList();
+
+            var goalCards = _manager.Cards.GetMotivationGoalCards().
+                                     Select(x => x.Id).
+                                     ToList();
+
+            var ids = activityCards.Concat(goalCards).ToList();
+            _manager.SaveSchedule(_user.Id, ids);
+            _manager.SaveSchedule(_user.Id, ids);
+            _manager.SaveSchedule(_user.Id, ids);
+
+            // act
+            var schedules = _manager.GetSchedules(_user.Id).ToList();
+            
+            // assert
+            Assert.AreEqual(3, schedules.Count);
+        }
+
+        [Test]
+        public void UpdateScheduleWithDeletingCards()
+        {
+            // arrange
+            var activityCards = _manager.Cards.GetActivityCards().
+                                         Select(x => x.Id).
+                                         ToList();
+
+            var goalCards = _manager.Cards.GetMotivationGoalCards().
+                                     Select(x => x.Id).
+                                     ToList();
+
+            var ids = activityCards.Concat(goalCards).ToList();
+            var scheduleId = _manager.SaveSchedule(_user.Id, ids);
+            var addedSchedule = _manager.GetSchedule(scheduleId);
+
+            // act
+            var newCardIds = _manager.GetCardIds(scheduleId).ToList();
+            newCardIds.RemoveAt(2);
+            _manager.UpdateSchedule(scheduleId, newCardIds);
+            addedSchedule = _manager.GetSchedule(scheduleId);
+
+            // assert
+            Assert.AreEqual(3, addedSchedule.ScheduleItems.Count);
+            Assert.AreEqual(activityCards[0], addedSchedule.ScheduleItems[0].CardId);
+            Assert.AreEqual(activityCards[1], addedSchedule.ScheduleItems[1].CardId);
+            Assert.AreEqual(goalCards[0], addedSchedule.ScheduleItems[2].CardId);
+        }
+
+        [Test]
+        public void UpdateScheduleWithAddingCards()
+        {
+            // arrange
+            var activityType = _manager.Cards.CardTypes.GetActivityCardType();
+            Card card = new Card()
+            {
+                PhotoPath = "path/to/photo5.jpg",
+                CardTypeId = activityType.Id
+            };
+
+            var activityCards = _manager.Cards.GetActivityCards().
+                                         Select(x => x.Id).
+                                         ToList();
+
+            var goalCards = _manager.Cards.GetMotivationGoalCards().
+                                     Select(x => x.Id).
+                                     ToList();
+
+            var ids = activityCards.Concat(goalCards).ToList();
+            var scheduleId = _manager.SaveSchedule(_user.Id, ids);
+            var addedSchedule = _manager.GetSchedule(scheduleId);
+
+            // act
+            var newCardId = _manager.Cards.SaveCard(card);
+
+            activityCards = _manager.Cards.GetActivityCards().
+                                     Select(x => x.Id).
+                                     ToList();
+            ids = activityCards.Concat(goalCards).ToList();
+
+            _manager.UpdateSchedule(scheduleId, ids);
+            addedSchedule = _manager.GetSchedule(scheduleId);
+
+            // assert
+            Assert.AreEqual(5, addedSchedule.ScheduleItems.Count);
+            Assert.AreEqual(activityCards[0], addedSchedule.ScheduleItems[0].CardId);
+            Assert.AreEqual(activityCards[1], addedSchedule.ScheduleItems[1].CardId);
+            Assert.AreEqual(activityCards[2], addedSchedule.ScheduleItems[2].CardId);
+            Assert.AreEqual(activityCards[3], addedSchedule.ScheduleItems[3].CardId);
+            Assert.AreEqual(goalCards[0], addedSchedule.ScheduleItems[4].CardId);
+        }
+
+        [Test]
+        public void ComplexUpdateSchedule()
+        {
+            // arrange
+            var activityType = _manager.Cards.CardTypes.GetActivityCardType();
+            Card card = new Card()
+            {
+                PhotoPath = "path/to/photo5.jpg",
+                CardTypeId = activityType.Id
+            };
+
+            var activityCards = _manager.Cards.GetActivityCards().
+                                         Select(x => x.Id).
+                                         ToList();
+
+            var goalCards = _manager.Cards.GetMotivationGoalCards().
+                                     Select(x => x.Id).
+                                     ToList();
+
+            var ids = activityCards.Concat(goalCards).ToList();
+            var scheduleId = _manager.SaveSchedule(_user.Id, ids);
+            var addedSchedule = _manager.GetSchedule(scheduleId);
+
+            // act
+            var newCardId = _manager.Cards.SaveCard(card);
+            ids = _manager.GetCardIds(scheduleId).ToList();
+            ids.Insert(1, newCardId);
+            ids.RemoveAt(3);
+            _manager.UpdateSchedule(scheduleId, ids);
+            addedSchedule = _manager.GetSchedule(scheduleId);
+
+            // assert
+            Assert.AreEqual(4, addedSchedule.ScheduleItems.Count);
+            Assert.AreEqual(1, addedSchedule.ScheduleItems[0].CardId);
+            Assert.AreEqual(5, addedSchedule.ScheduleItems[1].CardId);
+            Assert.AreEqual(2, addedSchedule.ScheduleItems[2].CardId);
+            Assert.AreEqual(4, addedSchedule.ScheduleItems[3].CardId);
+        }
+
+        [Test]
+        public void DeleteSchedule()
+        {
+            // arrange
 
             // act
 
             // assert
+        }
+
+        [Test]
+        public void FinishSchedule()
+        {
+            
+        }
+
+        [Test]
+        public void CompleteSchedule()
+        {
+            
         }
     }
 }
