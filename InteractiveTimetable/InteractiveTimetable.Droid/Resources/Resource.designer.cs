d5bd39f95974e9aaa19c4770b0bde243ee54c1db
Александр Петренко <gafk555@gmail.com>
2017-03-15 14:55:39 +0300

    Add frame around user photo

--- InteractiveTimetable/InteractiveTimetable.Droid/Resources/Resource.designer.cs
+++ InteractiveTimetable/InteractiveTimetable.Droid/Resources/Resource.designer.cs
@@ -103,8 +103,8 @@ namespace InteractiveTimetable.Droid
 			// aapt resource value: 0x7f050008
 			public const int linearLayout1 = 2131034120;
 			
-			// aapt resource value: 0x7f05000a
-			public const int linearLayout2 = 2131034122;
+			// aapt resource value: 0x7f05000b
+			public const int linearLayout2 = 2131034123;
 			
 			// aapt resource value: 0x7f050003
 			public const int main_landscape = 2131034115;
@@ -118,17 +118,20 @@ namespace InteractiveTimetable.Droid
 			// aapt resource value: 0x7f050004
 			public const int user_details = 2131034116;
 			
-			// aapt resource value: 0x7f05000c
-			public const int user_fpname = 2131034124;
+			// aapt resource value: 0x7f05000d
+			public const int user_fpname = 2131034125;
 			
-			// aapt resource value: 0x7f05000b
-			public const int user_lastname = 2131034123;
+			// aapt resource value: 0x7f05000c
+			public const int user_lastname = 2131034124;
 			
 			// aapt resource value: 0x7f050002
 			public const int user_list = 2131034114;
 			
 			// aapt resource value: 0x7f050009
-			public const int user_photo = 2131034121;
+			public const int user_list_photo_frame = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int user_photo = 2131034122;
 			
 			// aapt resource value: 0x7f050007
 			public const int user_recycler_view = 2131034119;
