b950072a69914853e20da96fbe6f774f05f57aac
Александр Петренко <gafk555@gmail.com>
2017-03-16 16:40:14 +0300

    Fix bug with creating new user list fragments
    New user list fragments were creating every time device configuration changes.

--- InteractiveTimetable/InteractiveTimetable.Droid/UserInterfaceLayer/ManagementActivity.cs
+++ InteractiveTimetable/InteractiveTimetable.Droid/UserInterfaceLayer/ManagementActivity.cs
@@ -6,18 +6,35 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
     [Activity(Label = "Management", MainLauncher = true)]
     public class ManagementActivity : Activity
     {
+        public static readonly string UserListFragmentTag = "user_list_fragment";
+
+        private UserListFragment _userListFragment;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
 			SetContentView (Resource.Layout.management);
 
-            var userListFragment = UserListFragment.NewInstance();
+            AddUserListFragment();
+        }
 
-            var fragmentManager = FragmentManager.BeginTransaction();
-            fragmentManager.Add(Resource.Id.user_list, userListFragment);
-            fragmentManager.SetTransition(FragmentTransit.FragmentFade);
-            fragmentManager.Commit();
+        private void AddUserListFragment()
+        {
+            /* Trying to find fragment */
+            _userListFragment = FragmentManager.FindFragmentByTag(UserListFragmentTag) 
+                as UserListFragment;
+
+            /* Creting a new one if not exist */
+            if (_userListFragment == null)
+            {
+                _userListFragment = UserListFragment.NewInstance();
+
+                var fragmentManager = FragmentManager.BeginTransaction();
+                fragmentManager.Replace(Resource.Id.user_list, _userListFragment, UserListFragmentTag);
+                fragmentManager.SetTransition(FragmentTransit.FragmentFade);
+                fragmentManager.Commit();
+            }
         }
     }
 }
