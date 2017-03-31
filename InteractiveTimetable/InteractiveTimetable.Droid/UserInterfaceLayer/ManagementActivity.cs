56378a704605f5f298dfe4ccca3077c15c894824
Александр Петренко <gafk555@gmail.com>
2017-03-31 14:12:43 +0300

    Add regions for ManagementActivity

--- InteractiveTimetable/InteractiveTimetable.Droid/UserInterfaceLayer/ManagementActivity.cs
+++ InteractiveTimetable/InteractiveTimetable.Droid/UserInterfaceLayer/ManagementActivity.cs
@@ -9,21 +9,29 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
     [Activity(Label = "Management", MainLauncher = true)]
     public class ManagementActivity : Activity
     {
+        #region Constants
         private static readonly string UserIdKey = "current_user_id";
+        #endregion
 
+        #region Fragments
         private UserListFragment _userListFragment;
         private UserDetailsFragment _userDetailsFragment;
         private UserDetailsEditFragment _userDetailsEditFragment;
+        #endregion
 
+        #region Internal Variables
         private int _currentUserId;
-
         private bool _isWideScreenDevice;
+        #endregion
 
+        #region Methods
+
+        #region Event Handlers
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-			SetContentView (Resource.Layout.management);
+            SetContentView(Resource.Layout.management);
 
             /* Hide keyboard */
             Window.SetSoftInputMode(SoftInput.StateAlwaysHidden);
@@ -32,59 +40,9 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
             var layout = FindViewById<LinearLayout>(Resource.Id.main_landscape);
             _isWideScreenDevice = layout != null && layout.Visibility == ViewStates.Visible;
 
+            /* Setting fragments */
             AddUserListFragment();
             AddUserDetailsFragment();
-        }
-
-        private void AddUserListFragment()
-        {
-            /* Trying to find fragment */
-            _userListFragment = FragmentManager.FindFragmentByTag(UserListFragment.FragmentTag) 
-                as UserListFragment;
-
-            /* Creting a new one if not exist */
-            if (_userListFragment == null)
-            {
-                _userListFragment = UserListFragment.NewInstance();
-                _userListFragment.ListItemClicked += OnUserListItemClicked;
-                _userListFragment.AddUserButtonClicked += OnAddUserButtonClicked;
-
-                var fragmentManager = FragmentManager.BeginTransaction();
-                fragmentManager.Replace(
-                        Resource.Id.user_list, 
-                        _userListFragment, 
-                        UserListFragment.FragmentTag
-                    );
-
-                fragmentManager.SetTransition(FragmentTransit.FragmentFade);
-                fragmentManager.Commit();
-            }
-        }
-
-        private void AddUserDetailsFragment()
-        {
-            if (_isWideScreenDevice)
-            {
-                _userDetailsFragment = FragmentManager.FindFragmentByTag(UserDetailsFragment.FragmentTag)
-                        as UserDetailsFragment;
-
-                if (_userDetailsFragment == null)
-                {
-                    _currentUserId = _userListFragment.GetFirstUserId();
-                    _userDetailsFragment = UserDetailsFragment.NewInstance(_currentUserId);
-                    _userDetailsFragment.EditButtonClicked += OnEditUserButtonClicked;
-
-                    var fragmentManager = FragmentManager.BeginTransaction();
-                    fragmentManager.Replace(
-                            Resource.Id.user_details,
-                            _userDetailsFragment,
-                            UserDetailsFragment.FragmentTag
-                        );
-
-                    fragmentManager.SetTransition(FragmentTransit.FragmentFade);
-                    fragmentManager.Commit();
-                }
-            }
         }
 
         protected override void OnDestroy()
@@ -96,28 +54,6 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
             base.OnDestroy();
         }
 
-        public void OnAddUserButtonClicked()
-        {
-            var editUserDetails = FragmentManager.FindFragmentByTag(UserDetailsEditFragment.FragmentTag)
-                        as UserDetailsEditFragment;
-
-            if (editUserDetails == null)
-            {
-                var editUserDetailsFragment = UserDetailsEditFragment.NewInstance(0);
-
-                var fragmentManager = FragmentManager.BeginTransaction();
-                fragmentManager.Replace(
-                        Resource.Id.user_details,
-                        editUserDetailsFragment,
-                        UserDetailsEditFragment.FragmentTag
-                    );
-
-                fragmentManager.SetTransition(FragmentTransit.FragmentFade);
-                fragmentManager.AddToBackStack(UserDetailsEditFragment.FragmentTag);
-                fragmentManager.Commit();
-            }
-        }
-
         public void OnUserListItemClicked(int userId)
         {
             if (_isWideScreenDevice)
@@ -125,7 +61,7 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
                 _userDetailsFragment = FragmentManager.FindFragmentByTag(UserDetailsFragment.FragmentTag)
                         as UserDetailsFragment;
 
-                if (_userDetailsFragment == null  || _currentUserId != userId)
+                if (_userDetailsFragment == null || _currentUserId != userId)
                 {
                     _userDetailsFragment = UserDetailsFragment.NewInstance(userId);
                     _userDetailsFragment.EditButtonClicked += OnEditUserButtonClicked;
@@ -173,5 +109,83 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
                 fragmentManager.Commit();
             }
         }
+
+        public void OnAddUserButtonClicked()
+        {
+            _userDetailsEditFragment = FragmentManager.FindFragmentByTag(UserDetailsEditFragment.FragmentTag)
+                        as UserDetailsEditFragment;
+
+            if (_userDetailsEditFragment == null)
+            {
+                _userDetailsEditFragment = UserDetailsEditFragment.NewInstance(0);
+
+                var fragmentManager = FragmentManager.BeginTransaction();
+                fragmentManager.Replace(
+                        Resource.Id.user_details,
+                        _userDetailsEditFragment,
+                        UserDetailsEditFragment.FragmentTag
+                    );
+
+                fragmentManager.SetTransition(FragmentTransit.FragmentFade);
+                fragmentManager.AddToBackStack(UserDetailsEditFragment.FragmentTag);
+                fragmentManager.Commit();
+            }
+        }
+        #endregion
+
+        #region Other Methods
+        private void AddUserListFragment()
+        {
+            /* Trying to find fragment */
+            _userListFragment = FragmentManager.FindFragmentByTag(UserListFragment.FragmentTag)
+                as UserListFragment;
+
+            /* Creting a new one if not exist */
+            if (_userListFragment == null)
+            {
+                _userListFragment = UserListFragment.NewInstance();
+                _userListFragment.ListItemClicked += OnUserListItemClicked;
+                _userListFragment.AddUserButtonClicked += OnAddUserButtonClicked;
+
+                var fragmentManager = FragmentManager.BeginTransaction();
+                fragmentManager.Replace(
+                        Resource.Id.user_list,
+                        _userListFragment,
+                        UserListFragment.FragmentTag
+                    );
+
+                fragmentManager.SetTransition(FragmentTransit.FragmentFade);
+                fragmentManager.Commit();
+            }
+        }
+
+        private void AddUserDetailsFragment()
+        {
+            if (_isWideScreenDevice)
+            {
+                _userDetailsFragment = FragmentManager.FindFragmentByTag(UserDetailsFragment.FragmentTag)
+                        as UserDetailsFragment;
+
+                if (_userDetailsFragment == null)
+                {
+                    _currentUserId = _userListFragment.GetFirstUserId();
+                    _userDetailsFragment = UserDetailsFragment.NewInstance(_currentUserId);
+                    _userDetailsFragment.EditButtonClicked += OnEditUserButtonClicked;
+
+                    var fragmentManager = FragmentManager.BeginTransaction();
+                    fragmentManager.Replace(
+                            Resource.Id.user_details,
+                            _userDetailsFragment,
+                            UserDetailsFragment.FragmentTag
+                        );
+
+                    fragmentManager.SetTransition(FragmentTransit.FragmentFade);
+                    fragmentManager.Commit();
+                }
+            }
+        }
+        #endregion
+
+        #endregion
     }
 }
