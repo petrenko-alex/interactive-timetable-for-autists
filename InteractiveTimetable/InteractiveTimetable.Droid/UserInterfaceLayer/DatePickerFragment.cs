594c1fe61b127da73328755adf50644d2a7b352f
Александр Петренко <gafk555@gmail.com>
2017-04-08 15:54:49 +0300

    Polish DatePickerFragment

--- InteractiveTimetable/InteractiveTimetable.Droid/UserInterfaceLayer/DatePickerFragment.cs
+++ InteractiveTimetable/InteractiveTimetable.Droid/UserInterfaceLayer/DatePickerFragment.cs
@@ -1,14 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Util;
-using Android.Views;
 using Android.Widget;
 
 namespace InteractiveTimetable.Droid.UserInterfaceLayer
@@ -35,9 +28,11 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
             DateTime currentDate,
             Action<DateTime> onDateSelected)
         {
-            DatePickerFragment fragment = new DatePickerFragment();
-            fragment._dateSelectedHandler = onDateSelected;
-            fragment._currentDate = currentDate;
+            var fragment = new DatePickerFragment
+            {
+                _dateSelectedHandler = onDateSelected,
+                _currentDate = currentDate
+            };
 
             return fragment;
         }
@@ -46,11 +41,13 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
         #region Event Handlers
         public override Dialog OnCreateDialog(Bundle savedInstanceState)
         {
-            DatePickerDialog dialog = new DatePickerDialog(Activity,
-                                                           this,
-                                                           _currentDate.Year,
-                                                           _currentDate.Month - 1,
-                                                           _currentDate.Day);
+            var dialog = new DatePickerDialog(
+                Activity,
+                this,
+                _currentDate.Year,
+                _currentDate.Month - 1,
+                _currentDate.Day
+            );
             dialog.SetCanceledOnTouchOutside(true);
 
             return dialog;
@@ -58,7 +55,7 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
 
         public void OnDateSet(DatePicker view, int year, int month, int day)
         {
-            DateTime selectedDate = new DateTime(year, month + 1, day);
+            var selectedDate = new DateTime(year, month + 1, day);
             _dateSelectedHandler(selectedDate);
         }
 
