031894b782a8f89539606fbb36852061e45c0a51
Александр Петренко <gafk555@gmail.com>
2017-05-07 08:43:51 +0300

    Set diagnostic dialog data on start

--- InteractiveTimetable/InteractiveTimetable.Droid/UserInterfaceLayer/DiagnosticDialogFragment.cs
+++ InteractiveTimetable/InteractiveTimetable.Droid/UserInterfaceLayer/DiagnosticDialogFragment.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Linq;
 using Android.App;
 using Android.OS;
 using Android.Views;
 using Android.Widget;
+using Com.Michaelmuenzer.Android.Scrollablennumberpicker;
 using InteractiveTimetable.BusinessLayer.Models;
 
 namespace InteractiveTimetable.Droid.UserInterfaceLayer
@@ -70,19 +72,12 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
             /* Set data to widgets */
             if (_diagnostic != null)
             {
-                Dialog.SetTitle(GetString(Resource.String.editing_diagnostic));
-
-                /* Adjust widgets */
-                _applyButton.Text = GetString(Resource.String.edit_button);
+                LoadDiagnostic(view);
 
             }
             else
             {
-                Dialog.SetTitle(GetString(Resource.String.adding_diagnostic));
-
-                var now = DateTime.Now;
-                _diagnosticDate.Text = now.ToString(DateFormat);
-                _diagnosticTime.Text = now.ToString(TimeFormat);
+                InitializeForNewDiagnostic();
             }
 
             return view;
@@ -96,6 +91,55 @@ namespace InteractiveTimetable.Droid.UserInterfaceLayer
         private void OnApplyButtonClicked(object sender, EventArgs e)
         {
             throw new NotImplementedException();
+        }
+
+        private void LoadDiagnostic(View view)
+        {
+            /* Get data */
+            var grades = _diagnostic.CriterionGrades.Select(x => x.Grade).ToList();
+
+            /* Set data to widgets */
+            Dialog.SetTitle(GetString(Resource.String.editing_diagnostic));
+            _applyButton.Text = GetString(Resource.String.edit_button);
+
+            /* Set dates */
+            _diagnosticDate.Text = _diagnostic.Date.ToString(DateFormat);
+            _diagnosticTime.Text = _diagnostic.Date.ToString(TimeFormat);
+
+            /* Set point grades */
+            int numberOfGrades = 17;
+            for (int i = 0; i < numberOfGrades; ++i)
+            {
+                string gradeId = "grade_" + (i + 1);
+                int id = Activity.Resources.GetIdentifier(gradeId, "id", Activity.PackageName);
+                var gradeView = view.FindViewById<ScrollableNumberPicker>(id);
+                gradeView.Value = int.Parse(grades[i]);
+            }
+
+            /* Set tick grade */
+            string lastGrade = grades.Last();
+            for (int i = 0; i < lastGrade.Length; ++i)
+            {
+                string gradeId = "grade_18_" + (i + 1);
+
+                if (lastGrade[i] == '1')
+                {
+                    int id = Activity.Resources.GetIdentifier(gradeId, "id", Activity.PackageName);
+                    var gradeView = view.FindViewById<CheckBox>(id);
+                    gradeView.Checked = true;
+                }
+            }
+        }
+
+        private void InitializeForNewDiagnostic()
+        {
+            /* Set titles */
+            Dialog.SetTitle(GetString(Resource.String.adding_diagnostic));
+
+            /* Set dates */
+            var now = DateTime.Now;
+            _diagnosticDate.Text = now.ToString(DateFormat);
+            _diagnosticTime.Text = now.ToString(TimeFormat);
         }
     }
 }
