4c3493546ecb17de5ed706dbabc6ee2f9c80b31b
Александр Петренко <gafk555@gmail.com>
2017-05-14 11:00:06 +0300

    Add BitmapHelper methods to load images async

--- InteractiveTimetable/InteractiveTimetable.Droid/ApplicationLayer/BtimapHelper.cs
+++ InteractiveTimetable/InteractiveTimetable.Droid/ApplicationLayer/BtimapHelper.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Android.Graphics;
 
 namespace InteractiveTimetable.Droid.ApplicationLayer
@@ -35,6 +36,64 @@ namespace InteractiveTimetable.Droid.ApplicationLayer
             Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
 
             return resizedBitmap;
+        }
+
+        public static async Task<Bitmap> LoadScaledDownBitmapForDisplayAsync(
+            this string fileName,
+            int reqWidth,
+            int reqHeight)
+        {
+            /* Get options of image */
+            BitmapFactory.Options options = await GetBitmapOptionsOfImageAsync(fileName);
+
+            /* Calculate inSampleSize */
+            options.InSampleSize = CalculateInSampleSize(options, reqWidth, reqHeight);
+
+            /* Decode bitmap with inSampleSize set */
+            options.InJustDecodeBounds = false;
+
+            return await BitmapFactory.DecodeFileAsync(fileName, options);
+        }
+
+        private static async Task<BitmapFactory.Options> GetBitmapOptionsOfImageAsync(string fileName)
+        {
+            var options = new BitmapFactory.Options
+            {
+                InJustDecodeBounds = true
+            };
+
+            /* The result will be null because InJustDecodeBounds == true */
+            var result = await BitmapFactory.DecodeFileAsync(fileName, options);
+
+            return options;
+        }
+
+        private static int CalculateInSampleSize(
+            BitmapFactory.Options options, 
+            int reqWidth, 
+            int reqHeight)
+        {
+            /* Raw height and width of image */
+            float height = options.OutHeight;
+            float width = options.OutWidth;
+            double inSampleSize = 1D;
+
+            if (height > reqHeight || width > reqWidth)
+            {
+                int halfHeight = (int) (height / 2);
+                int halfWidth = (int) (width / 2);
+
+                /* 
+                 * Calculate a inSampleSize that is a power of 2 - 
+                 * the decoder will use a value that is a power of two anyway. 
+                 */
+                while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth)
+                {
+                    inSampleSize *= 2;
+                }
+            }
+
+            return (int) inSampleSize;
         }
     }
 }
